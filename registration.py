348a840d945db1d00cd94669841ae9afb1f8d29e
Pola Schwöbel <pola@founders.as>
2018-03-31 18:49:28 +0200

    efficient implementation of the prior using the large evaluation matrix (instead of recomputing Gram matrix)

--- registration.py
+++ registration.py
@@ -1,6 +1,8 @@
 import numpy as np
 import vector_fields
 import forward_euler
+import utils
+import os
 import math
 
 
@@ -27,23 +29,28 @@ def likelihood(im1, im2, trafo, evaluation_points, sigma=1):
     return log_likelihood
 
 
-#def efficient_prior(S, alpha, kernel_res=100, eval_res=50, sigma=1): # or V directly for efficiency?
-#    # here it will be neccessary to recover the right entries of the evaluation matrix!
-#    print(S.shape)
-#    print(kernel_res//eval_res)
-#    rows = np.array([[i, i+1, i+2] for i in range(0, S.shape[0], (kernel_res//eval_res + 3)*3)]).flatten()
-#    print(rows[0:10], rows[-10:])
-#    S = S[rows, :]
-#    print(S.shape, alpha.shape)
-#    norm = alpha.T.dot(S.dot(alpha))
-#    log_likelihood = -norm/(2*sigma**2) + math.log(1/(sigma*math.sqrt(2*math.pi)))
-#    return log_likelihood
-
-
-def prior(gram_matrix, alpha, kernel_res=100, eval_res=50, sigma=1): # or V directly for efficiency?
-    norm = alpha.T.dot(gram_matrix.dot(alpha))
+def prior(S, alpha, img_shape, kernel_res, eval_res, sigma=1):
+    # find indices to recover gram matrix from large evaluation matrix
+    d = 3
+    resratio = kernel_res//eval_res
+    eval_x_dim = img_shape[0]//eval_res + 1
+    eval_y_dim = img_shape[1]//eval_res + 1
+    eval_z_dim = img_shape[2]//eval_res + 1
+    lowresrows = np.array([range(d*i*eval_y_dim*eval_x_dim, d*(i+1)*eval_y_dim*eval_x_dim)
+                          for i in range(0, eval_z_dim, resratio)]).flatten()
+    midresrows = np.array([range(d*i*eval_x_dim, d*(i+1)*eval_x_dim)
+                          for i in range(0, eval_z_dim*eval_y_dim, resratio)]).flatten()
+    highresrows = np.array([range(d*i, d*(i+1))
+                           for i in range(0, eval_x_dim*eval_z_dim*eval_y_dim, resratio)]).flatten()
+    keep = list(set(lowresrows) & set(midresrows) & set(highresrows))
+    indices = np.zeros(S.shape[0])
+    indices[keep] = 1
+    indices = indices.astype(bool)
+    S = S[indices, :]
+    # actual computation of the prior
+    norm = alpha.T.dot(S.dot(alpha))
     log_likelihood = -norm/(2*sigma**2) + math.log(1/(sigma*math.sqrt(2*math.pi)))
-    return log_likelihood
+    return log_likelihood, S
 
 
 # final registration function for 3d
@@ -56,10 +63,16 @@ def find_transformation(im1, im2):
     print('Getting grid points.')
     kernel_grid = vector_fields.get_points_3d(im1, kernel_res)
     evaluation_points = vector_fields.get_points_3d(im1, eval_res)
-    # kernel matrix can alternatively be loaded if pre-computed
-    print('Computing S.')
-    S = vector_fields.evaluation_matrix(lambda x1, x2: vector_fields.kernel(x1, x2, c_sup), kernel_grid,
-                                        evaluation_points)
+
+    if os.path.exists('evaluation_matrices/example3D_100_200_50.npy'):
+        print('Loading evaluation matrix.')
+        S = utils.load_matrix('example3D_100_200_50.npy')
+    else:
+        print('Computing S.') # kernel matrix can alternatively be loaded if pre-computed
+        S = vector_fields.evaluation_matrix(lambda x1, x2: vector_fields.kernel(x1, x2, c_sup), kernel_grid,
+                                            evaluation_points)
+        utils.save_matrix(S, 'example3D_100_200_2.npy')
+
     # HMCS
     nxd = S.shape[1]
     print('Initialize with zero vector field.')
@@ -70,10 +83,7 @@ def find_transformation(im1, im2):
     print('Compute likelihood.')
     log_likelihood = likelihood(im1, im2, trafo, evaluation_points)
     print('log_likelihood:', log_likelihood, '. Now compute prior.')
-    # this is the easy and unefficient way to do it - see above
-    print('Compute gram matrix.')
-    gram_matrix = vector_fields.gram_matrix(lambda x1, x2: vector_fields.kernel(x1, x2, c_sup), kernel_grid,)
-    log_prior = prior(gram_matrix, alpha)
+    log_prior = prior(S, alpha, im1.shape, kernel_res, eval_res)
     print('log_prior:', log_prior, 'Done.')
     log_posterior = log_likelihood + log_prior
 
