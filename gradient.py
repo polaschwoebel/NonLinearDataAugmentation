2e8a4c328384d74782de0171ffeddc830b00c70d
Pola Schwöbel <pola@founders.as>
2018-04-17 15:26:14 +0200

    more parts of the gradient

--- gradient.py
+++ gradient.py
@@ -13,14 +13,18 @@ def dv_dphit(phi_t, kernels, alpha, c_sup=200):
         # get those kernel points where distance is smaller than 1, so kernel > 0
         dist_smaller_1 = np.where(distances[i, :] < 1)[0]
         alpha_indices = np.array([j for j in list(dist_smaller_1)])
+        #print(alpha_indices)
         alpha_indices = alpha_indices.reshape(len(alpha_indices))
         relevant_alpha = alpha[alpha_indices]
+        #print(relevant_alpha)
         sm = 0
         # compute sum over all alpha+kernels that have a nonzero contribution
-        for idx in range(len(relevant_alpha)):
-            diff = phi_t_i - kernels[idx]
-            sm += relevant_alpha[idx].reshape((3, 1)) * diff.reshape((1,3)) * (1 - np.linalg.norm(diff)/c_sup)**3
-            dv_dphit_i = -20/c_sup**2 * sm
+        for alpha_idx in alpha_indices:
+            diff = phi_t_i - kernels[alpha_idx]
+            #print(diff, alpha[alpha_idx])
+            #print('We are at alpha:', alpha_idx, 'x-phi is:', diff)
+            sm += alpha[alpha_idx].reshape((3,1)).dot(diff.reshape((1,3))) * (1 - np.linalg.norm(diff)/c_sup)**3
+        dv_dphit_i = -20/c_sup**2 * sm
         dv_dphit_diag.append(dv_dphit_i)
     return sparse.block_diag(dv_dphit_diag)
 
@@ -31,3 +35,11 @@ def next_du_dalpha(S, dv_dphit, prev_du_dalpha, step_size):
     identity = sparse.identity(m)
     du_dalpha = (identity + 1/p * dv_dphit).dot(prev_du_dalpha) + 1/p * S
     return du_dalpha
+
+
+def dposterior_dphit():
+    pass # TODO
+
+def dprior_dalpha(S, alpha):
+    alpha = alpha.flatten()
+    return sparse.csc_matrix(2*S.dot(alpha))
